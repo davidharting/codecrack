a253749c06040116ee3cfa13b63dac57f6308a6e
David Harting <davideharting@gmail.com>
2020-01-10 23:14:13 -0500

    Stripe an array to do sums in parallel

---
+++ paralell_sum.go
@@ -0,0 +1,27 @@
+package main
+
+const concurrency = 32
+
+// Sum calculates the sum of all the numbers provided
+func Sum(numbers []int) int {
+	sums := make(chan int, concurrency)
+
+	for i := 0; i < concurrency; i++ {
+		go func(initial int) {
+			sum := 0
+
+			for j := initial; j < len(numbers); j += concurrency {
+				sum += numbers[j]
+			}
+
+			sums <- sum
+		}(i)
+	}
+
+	finalSum := 0
+	for i := 0; i < concurrency; i++ {
+		finalSum += <-sums
+	}
+
+	return finalSum
+}
---
+++ paralell_sum_test.go
@@ -0,0 +1,19 @@
+package main
+
+import "testing"
+
+import "github.com/stretchr/testify/assert"
+
+func TestSum(t *testing.T) {
+	sum := Sum([]int{2, 50, 99, 14, 21})
+	assert.Equal(t, 186, sum, "It should sum a list of numbers")
+
+	sum = Sum([]int{})
+	assert.Equal(t, 0, sum, "An empty list should yield 0")
+
+	longList := []int{}
+	for i := 0; i < 10000; i++ {
+		longList = append(longList, 3)
+	}
+	assert.Equal(t, 30000, Sum(longList), "It should handle long lists.")
+}
